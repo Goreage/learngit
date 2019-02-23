4e86e7bf199cfe19578b979276d30b9d32e98c37
Goerage <3196791814@qq.com>
2019-02-23 22:36:42 +0800

    every push

---
+++ Test-c++/Second.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a = 3;
+	printf("T have %d dollare.\n",a);
+	printf("I want to buy:\na book.");
+	return 0;
+}
---
+++ Test-c++/Sizeof.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n1 = 10;
+	double f;
+	char c;
+	printf("%d,%d,%d,%d",sizeof(n1),sizeof(short),
+	sizeof(double),sizeof(c));
+	return 0;
+}
