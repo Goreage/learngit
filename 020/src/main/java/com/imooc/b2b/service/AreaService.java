560a102322ee3561397760c9d7f90c860d6d1379
Goerage <3196791814@qq.com>
2020-07-10 23:17:41 +0800

    删除文件夹

--- 020/src/main/java/com/imooc/b2b/service/AreaService.java
+++
@@ -1,9 +0,0 @@
-package com.imooc.b2b.service;
-
-import java.util.List;
-
-import com.imooc.b2b.entity.Area;
-
-public interface AreaService {
-	List<Area> getAreaList();
-}
--- 020/src/main/java/com/imooc/b2b/util/PageCalculator.java
+++
@@ -1,7 +0,0 @@
-package com.imooc.b2b.util;
-
-public class PageCalculator {
-	public static int calculateRowIndex(int pageIndex, int pageSize) {
-		return (pageIndex > 0) ? (pageIndex - 1) * pageSize : 0;
-	}
-}
